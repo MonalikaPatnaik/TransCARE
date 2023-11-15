3c3a1b5cd9e1b3af1d4962fa7b408fcfdd523a5d
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-11-16 00:06:22 +0530

    About US Section Done

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../Components/LandingPage/Navbar';
-import AboutSection from '../Components/LandingPage/AboutSection'
+import AboutSection from '../Components/LandingPage/AboutUs/AboutUs.js';
 import HeroSection from '../Components/LandingPage/HeroSection';
 import ServicesSection from '../Components/LandingPage/ServicesSection';
 import ContributorsSection from '../Components/LandingPage/ContributorsSection';
@@ -10,7 +10,7 @@ import Footer from '../Components/LandingPage/Footer';
 const Home = () => {
   return (
     <>
-         <Navbar/> 
+        <Navbar/> 
         <HeroSection />
         <AboutSection/>
         <ServicesSection />
