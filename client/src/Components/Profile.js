ad831eb85dd0d79281a098f28c05dccf108154ab
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-04-15 15:52:43 +0530

    Profile page updated

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Container, Grid, Avatar, Typography, TextField, Button } from '@mui/material';
 import Loading from './Loader/Loader';
+
 const Profile = () => {
-  const { user, isAuthenticated,isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -29,37 +30,41 @@ const Profile = () => {
       reader.readAsDataURL(file);
     }
   };
-if(isLoading){
-  return <Loading />
-}
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <Container sx={{ marginTop: '10%', boxShadow: '0 .5rem 1rem rgba(0,0,0,.15)', marginRight: '10%', marginLeft: '10%', marginBottom: '20%', border: '2px solid #FDA5A4', paddingBottom: '50px' }}>
+    <Container sx={{ marginTop: '10%', boxShadow: '0 .5rem 1rem rgba(0,0,0,.15)', marginRight: '10%', marginLeft: '10%', marginBottom: '20%', border: '2px solid #FDA5A4', paddingBottom: '50px', backgroundColor: '#FFF9C4' }}>
       {isAuthenticated && (
         <Grid container spacing={3} sx={{ boxShadow: '0 .5rem 1rem rgba(0,0,0,.15)', textAlign: 'center', marginTop: '50px', paddingTop: '20px', marginLeft: '8%', marginRight: '8%', marginBottom: '2%', borderRadius: '20px', backgroundColor: '#F5F0F0', maxWidth: '100%' }}>
           {isSmallScreen ? (
             <Grid item xs={12} md={4}>
               <Grid item xs={12} sx={{ textAlign: 'center' }}>
-                <Avatar src={selectedImage || user.picture} alt={user.name} style={{ width: '65%', height: 'auto', borderRadius: '50%', margin: 'auto' }} />
-                <TextField type="file" accept="image/*" variant="outlined" sx={{ marginTop: 3 }} onChange={handleImageChange} />
+                <Avatar src={selectedImage || user.picture} alt={user.name} style={{ width: '65%', height: 'auto', borderRadius: '50%', margin: 'auto', backgroundColor: '#FFD180' }} />
+                <TextField type="file" accept="image/*" variant="outlined" sx={{ marginTop: 3, backgroundColor: '#FFD180' }} onChange={handleImageChange} />
               </Grid>
               <Grid item xs={12}>
-                <Typography variant="h3">{user.nickname}</Typography>
-                <Typography>{user.email}</Typography>
+                <Typography variant="h3" style={{ color: '#F06292' }}>{user.nickname}</Typography>
+                <Typography style={{ color: '#F06292' }}>{user.email}</Typography>
               </Grid>
             </Grid>
           ) : (
             <>
               <Grid item md={4} textAlign="center">
-                <Avatar src={selectedImage || user.picture} alt={user.name} sx={{ width: 200, height: 200, margin: 'auto', borderRadius: '50%' }} />
-                <TextField 
-                type="file" 
-                accept="image/*" 
-                variant="outlined" 
-                sx={{ marginTop: 3 }} onChange={handleImageChange} />
+                <Avatar src={selectedImage || user.picture} alt={user.name} sx={{ width: 200, height: 200, margin: 'auto', borderRadius: '50%', backgroundColor: '#FFD180' }} />
+                <TextField
+                  type="file"
+                  accept="image/*"
+                  variant="outlined"
+                  sx={{ marginTop: 3, backgroundColor: '#FFD180' }}
+                  onChange={handleImageChange}
+                />
               </Grid>
               <Grid item md={8}>
-                <Typography variant="h4">{user.nickname}</Typography>
-                <Typography>{user.email}</Typography>
+                <Typography variant="h4" style={{ color: '#F06292' }}>{user.nickname}</Typography>
+                <Typography style={{ color: '#F06292' }}>{user.email}</Typography>
               </Grid>
             </>
           )}
